a3a44c6e44cbaca6a43ef75eda17d6a16021866c
AbyChan <crystal.tyan@gmail.com>
2016-05-08 00:28:59 +0800

    inital commit

---
+++ src/index.js
@@ -0,0 +1,29 @@
+'use strict';
+
+let org = require('org');
+
+let parser = new org.Parser();
+
+export default class NobbbParseOrg {
+    constructor() {
+        this.name = 'org';
+    }
+
+    check(file) {
+        return /\.org$/.test(file);
+    }
+    
+    parse(orgCode) {
+        let orgDocument = parser.parse(orgCode);
+        let orgHTMLDocument = orgDocument.convert(org.ConverterHTML, {
+            headerOffset: 1,
+            exportFromLineNumber: false,
+            suppressSubScriptHandling: false,
+            suppressAutoLink: false
+        });
+        return {
+            title: orgHTMLDocument.title,
+            content: orgHTMLDocument.contentHTML
+        };
+    }
+}
